62ac4993af44a820216c434fd7cbb8625e72696e
valentin <valentingueret06@gmail.com>
2023-03-01 09:52:22 +0100

    challenge

---
+++ Hangar.java
@@ -0,0 +1,12 @@
+public class Hangar {
+    
+    public static void main(String[] args) {
+
+        Car clio = new Car("Clio",61652); 
+        System.out.println(clio.doStuff());
+
+        Boat titanic  = new Boat("Titanic",8622); 
+        System.out.println(titanic.doStuff());
+ 
+    }
+}
